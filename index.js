31b85e7e2cd782004aa1293d012996dbc035c0c4
Brandon-Harris <brandonharris177@gmail.com>
2019-10-02 14:22:25 -0600

    server running

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // code away!
+
+const server = require('./server')
+
+port = 5000
+  
+// watch for connections on port 5000
+server.listen(port, () =>
+console.log(`Server running on http://localhost:${port}`)
+);
