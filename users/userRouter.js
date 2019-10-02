968213ad77271b0ccca9195faec55a6342c6f55a
Brandon-Harris <brandonharris177@gmail.com>
2019-10-02 15:17:44 -0600

    validate by id working

--- users/userRouter.js
+++ users/userRouter.js
@@ -20,13 +20,9 @@ router.get('/', (req, res) => {
     )
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', validateUserId, (req, res) => {
     UserDB.getById(req.params.id).then(user => {
-        if (user) {
             res.status(200).json(user)
-        } else {
-            res.status(404).json({Messgae: `user with given id does not exist`})
-        }
     }).catch(error =>
         res.status(500).json({error: `Server error could not get data error: ${error}`})
     )
@@ -40,28 +36,17 @@ router.delete('/:id', (req, res) => {
 
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', validateUserId, (req, res) => {
     const id = req.params.id
-    console.log(id)
-    UserDB.getById(id)
-    .then(user => {
-        console.log(`user`, user)
-        if(user) {
-            UserDB.update(id, req.body)
-           .then (response => {
-               if (response === 1) {
-                UserDB.getById(id)
-                .then(updatedUser =>
-                    res.status(200).json(updatedUser)
-                )
-               } else {
-                   res.status(500).json({error: 'error user not correctly updated'})
-               }
-           }).catch (error =>
-                res.status(500).json({error: `Server error could not update data error: ${error}`})
-           )
+    UserDB.update(id, req.body)
+    .then (response => {
+        if (response === 1) {
+        UserDB.getById(id)
+        .then(updatedUser =>
+            res.status(200).json(updatedUser)
+        )
         } else {
-            res.status(404).json({Messgae: `user with given id does not exist`})
+            res.status(500).json({error: 'error user not correctly updated'})
         }
     }).catch (error =>
         res.status(500).json({error: `Server error could not update data error: ${error}`})
@@ -71,7 +56,19 @@ router.put('/:id', (req, res) => {
 //custom middleware
 
 function validateUserId(req, res, next) {
-
+    const id = req.params.id
+    console.log(id)
+    UserDB.getById(id)
+    .then(user => {
+        console.log(`user`, user)
+        if(user) {
+            next();
+        } else {
+            res.status(404).json({Messgae: "invalid user id"})
+        }
+    }).catch (error =>
+        res.status(500).json({error: `Server error could not update data error: ${error}`})
+    )
 };
 
 function validateUser(req, res, next) {
